1b4c17cbd7ebcfb656bd17ab2d02ba38f918a466
AlexanderPokidin <a.pokidin@gmail.com>
2018-05-14 11:27:25 +0300

    Initial commit

---
+++ app/src/main/java/com/pokidin/a/shafa/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.pokidin.a.shafa
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.btn_camera)
+        button.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
