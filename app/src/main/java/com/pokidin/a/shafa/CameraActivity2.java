dbe2376065b59bd9f71e41f89ac8f5dd3621bb25
AlexanderPokidin <a.pokidin@gmail.com>
2018-05-15 18:20:11 +0300

    Class CameraHelper created. CameraManager uses

--- app/src/main/java/com/pokidin/a/shafa/CameraActivity2.java
+++ app/src/main/java/com/pokidin/a/shafa/CameraActivity2.java
@@ -3,25 +3,30 @@ package com.pokidin.a.shafa;
 import android.content.Context;
 import android.graphics.ImageFormat;
 import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.util.Size;
+import android.view.View;
+import android.widget.Button;
 
 import com.pokidin.a.shafa.camera.CameraHelper;
 
 public class CameraActivity2 extends AppCompatActivity{
 
-    final static String TAG = "CameraActivity2";
+    private final String TAG = "CameraActivity2";
+    private final int CAMERA1 = 0;
+    private final int CAMERA2 = 1;
 
     CameraManager mCameraManager = null;
     CameraHelper[] myCameras = null;
+
+    private CameraHelper[] mCameras = null;
+    private Button mButtonOpenCamera1 = null;
+    private Button mButtonOpenCamera2 = null;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -45,6 +50,23 @@ public class CameraActivity2 extends AppCompatActivity{
             Log.d(TAG, e.getMessage());
             e.printStackTrace();
         }
+
+        mButtonOpenCamera1 = findViewById(R.id.btnOpenCamera1);
+        mButtonOpenCamera2 = findViewById(R.id.btnOpenCamera2);
+
+        mButtonOpenCamera1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (myCameras[CAMERA1].isOpen()){
+                    myCameras[CAMERA2].closeCamera();
+                }
+                if (myCameras[CAMERA2] != null){
+                    if (!myCameras[CAMERA2].isOpen()){
+                        myCameras[CAMERA2].openCamera();
+                    }
+                }
+            }
+        });
     }
 }
 
--- app/src/main/java/com/pokidin/a/shafa/camera/CameraHelper.java
+++ app/src/main/java/com/pokidin/a/shafa/camera/CameraHelper.java
@@ -3,6 +3,7 @@ package com.pokidin.a.shafa.camera;
 import android.graphics.ImageFormat;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Build;
@@ -12,10 +13,58 @@ import android.util.Log;
 import android.util.Size;
 
 public class CameraHelper {
-    final static String TAG = "CameraHelper";
+    private final static String TAG = "CameraHelper";
 
     private CameraManager mCameraManager = null;
     private String mCameraID = null;
+    private CameraDevice mCameraDevice = null;
+
+    public boolean isOpen() {
+        if (mCameraDevice == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void openCamera() {
+        try {
+            mCameraManager.openCamera(mCameraID, mCameraCallback, null);
+        } catch (CameraAccessException | SecurityException e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    public void closeCamera() {
+        if (mCameraDevice != null){
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
+    }
+
+    private CameraDevice.StateCallback mCameraCallback = new CameraDevice.StateCallback() {
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            mCameraDevice = camera;
+            Log.i(TAG, "Open camera with id: " + mCameraDevice.getId());
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            mCameraDevice.close();
+            Log.i(TAG, "Disconnected camera with id: " + mCameraDevice.getId());
+            mCameraDevice = null;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            Log.i(TAG, "Error! Camera with id: " + mCameraDevice.getId() + " error: " + error);
+        }
+    };
 
     public CameraHelper(@NonNull CameraManager cameraManager, @NonNull String cameraID) {
         mCameraManager = cameraManager;
@@ -27,7 +76,7 @@ public class CameraHelper {
         CameraCharacteristics characteristics = null;
         try {
             String[] cameraList = mCameraManager.getCameraIdList();
-            for (String cameraID : cameraList){
+            for (String cameraID : cameraList) {
                 Log.i(TAG, "CameraID: " + cameraID);
 
                 characteristics = mCameraManager.getCameraCharacteristics(cameraID);
@@ -35,8 +84,8 @@ public class CameraHelper {
                         characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
 
                 Size[] sizesJPEG = configurationMap.getOutputSizes(ImageFormat.JPEG);
-                if (sizesJPEG != null){
-                    for (Size item : sizesJPEG){
+                if (sizesJPEG != null) {
+                    for (Size item : sizesJPEG) {
                         Log.i(TAG, "w: " + item.getWidth() + ", h: " + item.getHeight());
                     }
                 } else {
@@ -45,7 +94,6 @@ public class CameraHelper {
             }
         } catch (CameraAccessException e) {
             Log.d(TAG, e.getMessage());
-//            e.printStackTrace();
         }
     }
 }
